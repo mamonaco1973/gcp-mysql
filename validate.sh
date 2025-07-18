50b64a4b49c909f8f3fda73a5be8e34c43bec526
Mike Monaco <mamonaco1973@gmail.com>
2025-07-18 13:06:16 -0400

    finish validate.sh script

--- validate.sh
+++ validate.sh
@@ -4,11 +4,11 @@
 # Output pgweb URL and postgres DNS name
 #-------------------------------------------------------------------------------
 
-# PGWEB_IP=$(gcloud compute instances describe pgweb-vm \
-#   --zone=us-central1-a \
-#   --format='value(networkInterfaces[0].accessConfigs[0].natIP)')
+ PHPMYADMIN_IP=$(gcloud compute instances describe phpmyadmin-vm \
+   --zone=us-central1-a \
+   --format='value(networkInterfaces[0].accessConfigs[0].natIP)')
 
-# echo "NOTE: pgweb running at http://$PGWEB_IP"
+echo "NOTE: phpMyAdmin running at http://$PHPMYADMIN_IP"
 
 MYSQL_DNS="mysql.internal.db-zone.local"
 echo "NOTE: Hostname for mysql server is \"$MYSQL_DNS\""
